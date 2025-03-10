eceb6af0615b67e5e43dd4ddca808769e21b64ae
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2025-03-10 01:00:06 -0400

    create API for gift service

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -16,13 +16,12 @@ async function connectToDatabase() {
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
-
+    await client.connect();
+    
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    dbInstance = client.db(dbName);
+    //task 3:
+    return dbInstance    
 }
 
 module.exports = connectToDatabase;
